30cfad82ca6697c1d3c2d2148a53226418caea2f
SEC-MASUM <masum.jess42@gmail.com>
2022-03-31 01:03:01 +0600

    NotFound Page and route is added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import NotFound from "./components/NotFound/NotFound";
 import Restaurant from "./components/Restaurant/Restaurant";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Restaurant></Restaurant>} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   );
